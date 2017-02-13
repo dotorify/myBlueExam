7f15a1df56118da8cbb5528a024632e06d48e173
dotorify <dotorify@gmail.com>
2017-02-14 01:59:41 +0900

    add. 1240nara

--- 1240nara.js
+++ 1240nara.js
@@ -1,14 +1,3 @@
-# myBlueExam
-코드스쿼드 마스터즈코스 블루레벨 레벨테스트 공간입니다.
-
-
-## 문제1: 1240나라
-
-[문제 보기](https://github.com/code-squad/homepage/wiki/Blue-Exam#%EB%AC%B8%EC%A0%9C1-1240-%EB%82%98%EB%9D%BC)
-
-[codepen 코드 및 결과 보기](http://codepen.io/dotorify/pen/jyXRdZ)
-
-```js
 // 1240Nara library via IIFE
 (function () {
   'use strict';
@@ -130,16 +119,3 @@
   window.to1240 = to1240; // add to window
   window.toDec = toDec; // add to window
 }());
-
-console.log('10진법의 1 => ' + to1240(1) + ' => 10진법의 ' + toDec('1')); // 10진법의 1 => 1 => 10진법의 1
-console.log('10진법의 2 => ' + to1240(2) + ' => 10진법의 ' + toDec('2')); // 10진법의 2 => 2 => 10진법의 2
-console.log('10진법의 3 => ' + to1240(3) + ' => 10진법의 ' + toDec('4')); // 10진법의 3 => 4 => 10진법의 3
-console.log('10진법의 4 => ' + to1240(4) + ' => 10진법의 ' + toDec('10')); // 10진법의 4 => 10 => 10진법의 4
-console.log('10진법의 5 => ' + to1240(5) + ' => 10진법의 ' + toDec('11')); // 10진법의 5 => 11 => 10진법의 5
-console.log('10진법의 6 => ' + to1240(6) + ' => 10진법의 ' + toDec('12')); // 10진법의 6 => 12 => 10진법의 6
-console.log('10진법의 7 => ' + to1240(7) + ' => 10진법의 ' + toDec('14')); // 10진법의 7 => 14 => 10진법의 7
-console.log('10진법의 8 => ' + to1240(8) + ' => 10진법의 ' + toDec('20')); // 10진법의 8 => 20 => 10진법의 8
-console.log('10진법의 9 => ' + to1240(9) + ' => 10진법의 ' + toDec('21')); // 10진법의 9 => 21 => 10진법의 9
-console.log('10진법의 10 => ' + to1240(10) + ' => 10진법의 ' + toDec('22')); // 10진법의 10 => 22 => 10진법의 10
-console.log('문자열 aa => ' + to1240('aa') + ' => 1240나라 문자열이아닌 숫자 ' + toDec(11)); // 문자열 aa => -1 => 1240나라 문자열이아닌 숫자 -1
-```
